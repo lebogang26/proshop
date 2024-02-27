968829905dc5d4b65b419fffe74e4aee0a95750c
Lebogang Sekaleli <lebogangsekaleli@gmail.com>
2024-02-27 22:36:49 +0200

    Added custom styles and logo

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,15 +1,15 @@
-import {Container, Row, Col} from 'react-bootstrap'
+import { Container, Row, Col } from "react-bootstrap"
 
 const Footer = () => {
-    const currentYear = new Date().getFullYear()
+  const currentYear = new Date().getFullYear()
 
   return (
     <footer>
       <Container>
         <Row>
-            <Col className='text-center py-3'>
-                <p>ProShop &copy; {currentYear}</p>
-            </Col>
+          <Col className="text-center py-3">
+            <p>ProShop &copy; {currentYear}</p>
+          </Col>
         </Row>
       </Container>
     </footer>
