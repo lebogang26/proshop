12c003cdad67ef59c19867b93dd4a916f37b37a5
Lebogang Sekaleli <lebogangsekaleli@gmail.com>
2024-03-06 21:41:04 +0200

    List products

---
+++ frontend/src/components/Product.jsx
@@ -0,0 +1,20 @@
+import { Card } from "react-bootstrap"
+
+const Product = ({ product }) => {
+  return (
+    <Card className="py-3 p-3 rounded">
+      <a href={`/product/${product._id}`}>
+        <Card.Img src={product.image} varient="top" />
+      </a>
+      <Card.Body>
+        <a href={`/product/${product._id}`}> 
+          <Card.Title as="div">
+            <strong>{product.name}</strong>
+          </Card.Title>
+        </a>
+      </Card.Body>
+    </Card>
+  )
+}
+
+export default Product
