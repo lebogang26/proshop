45b2bbca3ff0c18efaacb022e9be718fc5eead33
Lebogang Sekaleli <lebogangsekaleli@gmail.com>
2024-03-19 22:48:48 +0200

    Fixed header component

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,32 +1,33 @@
-import { Navbar, Nav, Container } from "react-bootstrap"
-import { FaShoppingCart, FaUser } from "react-icons/fa"
-import logo from "../assets/logo.png"
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import { FaShoppingCart, FaUser } from 'react-icons/fa';
+import logo from '../assets/logo.png';
 
 const Header = () => {
   return (
-    <Header>
-      <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
+    <header>
+      <Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">
-            <img src={logo} alt="ProShop" />
+          <Navbar.Brand href='/'>
+            <img src={logo} alt='ProShop' />
             ProShop
           </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav class="ms-auto">
-              <Nav.Link href="/cart">
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ms-auto'>
+              <Nav.Link href='/cart'>
                 <FaShoppingCart /> Cart
               </Nav.Link>
 
-              <Nav.Link href="/login">
+              <Nav.Link href='/login'>
                 <FaUser /> Sign In
               </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
-    </Header>
-  )
-}
+    </header>
+  );
+};
 
-export default Header
+export default Header;
